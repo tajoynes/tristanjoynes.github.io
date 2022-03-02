06298643924d7036feb343757d75f7dcdd999ee3
Tristan Joynes <tristanjoynes@tristans-imac.attlocal.net>
2022-03-01 23:38:07 -0600

    Contact Form Added, Re-Edit

--- src/Main.js
+++ src/Main.js
@@ -21,7 +21,11 @@ import {
   AiOutlineArrowDown,
   AiOutlineEye,
   AiOutlineDownload,
+  AiFillForward,
+  AiFillPhone,
+  AiOutlineEnvironment,
 } from "react-icons/ai";
+import { IconContext } from "react-icons";
 
 function Main() {
   return (
@@ -45,25 +49,49 @@ function Main() {
                 <p className="home-content-sub">Fitness Enthusiast</p>
                 <p className="home-content-sub">and a dog dad </p>
                 <br></br>
-                <div className="btn-wrappe justify-content-around d-none d-md-flex">
-                  <div className="text-center">
-                    <Button className="contact-view-btn btn" href="#contact">
-                      Contact Me
-                      <AiOutlineMail
-                        size={20}
-                        style={{ marginBottom: "2.5px", marginLeft: "8px" }}
-                      />
-                    </Button>
-                  </div>
-                  <div className=" text-center">
-                    <Button className="project-view-btn btn" href="#projects">
-                      View Projects
-                      <AiOutlineArrowDown
-                        size={20}
-                        style={{ marginBottom: "2.5px", marginLeft: "8px" }}
-                      />
-                    </Button>
-                  </div>
+                <div className="btn-wrapper justify-content-around d-none d-md-flex">
+                  <IconContext.Provider value={{ className: "banner-icons" }}>
+                    <div className="text-center">
+                      <a href="#contact">
+                        <button className="contact-view-btn py-2 px-5">
+                          Contact Me
+                          <AiOutlineMail
+                            size={24}
+                            style={{ marginBottom: "3px", marginLeft: "8px" }}
+                          />
+                        </button>
+                      </a>
+                    </div>
+                    <div className=" text-center">
+                      <a href="#projects">
+                        <button className="project-view-btn py-2 px-5">
+                          View Projects
+                          <AiOutlineArrowDown
+                            size={24}
+                            style={{ marginBottom: "2.5px", marginLeft: "8px" }}
+                          />
+                        </button>
+                      </a>
+                    </div>
+                  </IconContext.Provider>
+                </div>
+                <div className="readmore-btn-wrapper d-md-none">
+                  <IconContext.Provider value={{ className: "banner-icons" }}>
+                    <div className="text-center">
+                      <a href="#about">
+                        <button className="contact-view-btn py-2 px-5">
+                          Read More
+                          <AiFillForward
+                            size={25}
+                            style={{
+                              marginBottom: "2.5px",
+                              marginLeft: "20px",
+                            }}
+                          />
+                        </button>
+                      </a>
+                    </div>
+                  </IconContext.Provider>
                 </div>
               </div>
             </Col>
@@ -120,8 +148,8 @@ function Main() {
               </div>
             </Col>
           </Row>
-          <Row className="d-none d-sm-block mb-2">
-            <Col className="prjctcard main">
+          <Row className="gap-3 mb-2">
+            <Col sm className="prjctcard main">
               <div className="prjct-wrapper">
                 <h5 className="prjct-heading">Project 1 Name</h5>
                 <p className="prjct-desc">
@@ -130,9 +158,7 @@ function Main() {
                 </p>
               </div>
             </Col>
-          </Row>
-          <Row className="d-none d-sm-block">
-            <Col className="prjctcard main">
+            <Col sm className="prjctcard main">
               <div className="prjct-wrapper">
                 <h5 className="prjct-heading">Project 1 Name</h5>
                 <p className="prjct-desc">
@@ -167,6 +193,7 @@ function Main() {
                 border: "1px solid #17B890",
                 height: "100%",
                 position: "relative",
+                borderRadius: "5px",
               }}
               fluid
             >
@@ -184,23 +211,34 @@ function Main() {
                   }}
                 />
                 <Card className="" style={{ height: "100%", width: "100%" }}>
-                  <img src={imgone} height="100%" width="auto" />
+                  <img src={imgone} height="auto" width="auto" />
                 </Card>
-                <div className="resume-btn-wrapper d-flex justify-content-evenly">
-                  <Button className="resume-btn text-nowrap">
-                    View resume{" "}
-                    <AiOutlineEye
-                      size={20}
-                      style={{ marginBottom: "2.5px", marginLeft: "8px" }}
-                    />
-                  </Button>
-                  <Button className="resume-btn text-nowrap">
-                    Download resume{" "}
-                    <AiOutlineDownload
-                      size={20}
-                      style={{ marginBottom: "2.5px", marginLeft: "8px" }}
-                    />
-                  </Button>
+                <hr
+                  style={{
+                    backgroundColor: "#17B890",
+                    height: "2px",
+                    opacity: "1",
+                    marginLeft: "20px",
+                    marginRight: "20px",
+                  }}
+                />
+                <div className="resume-btn-wrapper d-flex justify-content-evenly px-3 mt-2">
+                  <IconContext.Provider value={{ className: "resume-icons" }}>
+                    <button className="resume-btn text-nowrap p-2">
+                      View resume{" "}
+                      <AiOutlineEye
+                        size={20}
+                        style={{ marginBottom: "2.5px", marginLeft: "8px" }}
+                      />
+                    </button>
+                    <button className="resume-btn text-nowrap">
+                      Download resume{" "}
+                      <AiOutlineDownload
+                        size={20}
+                        style={{ marginBottom: "2.5px", marginLeft: "8px" }}
+                      />
+                    </button>
+                  </IconContext.Provider>
                 </div>
               </div>
             </Container>
@@ -209,31 +247,82 @@ function Main() {
       </section>
 
       <section id="contact">
-        <Row className="contact-content-wrapper">
-          <Col className="contact-form-wrapper p-5">
-            <Form>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
+        <Container className="contact-content-wrapper pt-5" fluid>
+          <Row className="">
+            <Col>
+              <div className="resume-btn-wrapper d-flex justify-content-evenly px-3 mt-2">
+                <button className="resume-btn text-nowrap p-2">
+                  Get In Touch
+                </button>
+              </div>
+              <hr
+                style={{
+                  backgroundColor: "#17B890",
+                  height: "2px",
+                  opacity: "1",
+                  marginLeft: "30%",
+                  marginRight: "30%",
+                }}
+              />
+              <p className="px-5 text-center">
+                Lorem ipsum dolor sit amet, consect adipisic elit, sed do
+                eiusmod tempor incididunt ut labore et d magna aliqua enim sed
+                do sit.
+              </p>
+            </Col>
+          </Row>
+          <Row className="pb-5">
+            <Col className="contact-form-wrapper pt-5 d-none d-md-block">
+              <Card>
+                <AiFillPhone />
+                Contact Me:<br></br> +1 (901) 496-7584
+              </Card>
+              <Card>
+                <AiOutlineMail />
+                Email Address<br></br>tajoynes@gmail.com
+              </Card>
+              <Card>
+                <AiOutlineEnvironment />
+                Location<br></br>Houston, TX
+              </Card>
+            </Col>
+            <Col>
+              <Form>
+                <Row className="mb-3">
+                  <Form.Group as={Col} controlId="formGridEmail">
+                    <Form.Control type="email" placeholder="Name" />
+                  </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Form>
-          </Col>
-          <Col></Col>
-        </Row>
+                  <Form.Group as={Col} controlId="formGridPassword">
+                    <Form.Control type="password" placeholder="Email" />
+                  </Form.Group>
+                </Row>
+
+                <Row className="mb-3">
+                  <Form.Group as={Col} controlId="formGridEmail">
+                    <Form.Control type="email" placeholder="Phone" />
+                  </Form.Group>
+
+                  <Form.Group as={Col} controlId="formGridPassword">
+                    <Form.Control type="password" placeholder="Subject" />
+                  </Form.Group>
+                </Row>
+
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlTextarea1"
+                >
+                  <Form.Control placeholder="Message" as="textarea" rows={3} />
+                </Form.Group>
+                <Row className="mb-3"></Row>
+                <button className="resume-btn text-nowrap p-2" type="submit">
+                  Send Message
+                </button>
+              </Form>
+            </Col>
+          </Row>
+          <p className="text-center">Copyright © 2020 All Rights Reserved.</p>
+        </Container>
       </section>
     </div>
   );
